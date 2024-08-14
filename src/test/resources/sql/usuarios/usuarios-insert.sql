c4d9e9aa9174ebb1d6e2684d8801753247c3bba8
Antonio-Romes <antonioromes1@hotmail.com>
2024-08-14 09:25:47 -0300

    Configurar o ambiente de testes

---
+++ src/test/resources/sql/usuarios/usuarios-insert.sql
@@ -0,0 +1,4 @@
+
+insert into Usuarios (id,username,password,role) values(100,'ana@email.com','123456','ROLE_ADMIN');
+insert into Usuarios (id,username,password,role) values(101,'joão@email.com','123456','ROLE_CLIENTE');
+insert into Usuarios (id,username,password,role) values(102,'beatriz@email.com','123456','ROLE_CLIENTE');
