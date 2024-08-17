60159dd11dffc226211740eb450e8def7e72f6a5
Antonio-Romes <antonioromes1@hotmail.com>
2024-08-17 16:25:31 -0300

    Criando e mapeando a entidade Vaga e o Repositório.

---
+++ src/main/java/com/mballen/demo_park_api/repository/VagaRepository.java
@@ -0,0 +1,10 @@
+package com.mballen.demo_park_api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.mballen.demo_park_api.entity.Vaga;
+
+public interface VagaRepository extends JpaRepository<Vaga, Long>{
+
+    
+}  
