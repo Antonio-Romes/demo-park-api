1654113ec9f8423f366d682f7d180f32b9164532
Antonio-Romes <antonioromes1@hotmail.com>
2024-08-18 17:40:26 -0300

    Realizar o check-in de veiculo via postman

--- src/main/java/com/mballen/demo_park_api/web/dto/EstacionamentoResponseDto.java
+++ src/main/java/com/mballen/demo_park_api/web/dto/EstacionamentoResponseDto.java
@@ -4,9 +4,13 @@ package com.mballen.demo_park_api.web.dto;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import lombok.*;
 
 @Getter @Setter @AllArgsConstructor @NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class EstacionamentoResponseDto {
          
     private String placa; 
@@ -14,7 +18,11 @@ public class EstacionamentoResponseDto {
     private String modelo; 
     private String cor; 
     private String recibo; 
+
+    @JsonFormat(pattern = "yyyy-MM-dd hh:mm:ss")
     private LocalDateTime dataEntrada; 
+
+    @JsonFormat(pattern = "yyyy-MM-dd hh:mm:ss")
     private LocalDateTime dataSaida; 
     private String vagaCodigo; 
     private BigDecimal valor; 
