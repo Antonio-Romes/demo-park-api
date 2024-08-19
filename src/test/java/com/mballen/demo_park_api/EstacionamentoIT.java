a6e11fbc6b68ef57adeb72b625cca5ca54ab619b
Antonio-Romes <antonioromes1@hotmail.com>
2024-08-18 23:48:10 -0300

    Testando a operação de check-in com status 422 e 403.

--- src/test/java/com/mballen/demo_park_api/EstacionamentoIT.java
+++ src/test/java/com/mballen/demo_park_api/EstacionamentoIT.java
@@ -11,6 +11,7 @@ import org.springframework.test.web.reactive.server.WebTestClient;
 import com.mballen.demo_park_api.web.dto.EstacionamentoCreateDto;
 
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -30,6 +31,7 @@ public class EstacionamentoIT {
             testClient
                 .post()
                 .uri("/api/v1/estacionamentos/check-in")
+                .contentType(MediaType.APPLICATION_JSON)
                 .headers(JwtAuthentication.getHeaderAuthentication(testClient,"ana@email.com", "123456"))
                 .bodyValue(createDto)
                 .exchange()
@@ -38,10 +40,52 @@ public class EstacionamentoIT {
                 .expectBody()
                 .jsonPath("placa").isEqualTo("WER-1111")
                 .jsonPath("marca").isEqualTo("FIAT")
-                .jsonPath("modelo").isEqualTo("PALIO 1.0")
+                .jsonPath("modelo").isEqualTo("PALIO 1.0") 
                 .jsonPath("cor").isEqualTo("AZUL") 
                 .jsonPath("dataEntrada").exists()
                 .jsonPath("recibo").exists()
                 .jsonPath("vagaCodigo").exists();
-              }
+    }
+
+    @Test
+    public void criaCheckIn_ComRoleCliente_RetornarErrorComStatus403(){
+
+        EstacionamentoCreateDto createDto = EstacionamentoCreateDto.builder()
+            .placa("WER-1111").marca("FIAT").modelo("PALIO 1.0").cor("AZUL").clienteCpf("48810691016")
+            .build();
+
+            testClient
+                .post() 
+                .uri("/api/v1/estacionamentos/check-in")
+                .contentType(MediaType.APPLICATION_JSON)
+                .headers(JwtAuthentication.getHeaderAuthentication(testClient,"bia@email.com", "123456")) 
+                .bodyValue(createDto)
+                .exchange()
+                .expectStatus().isForbidden() 
+                .expectBody()
+                .jsonPath("status").isEqualTo("403")
+                .jsonPath("path").isEqualTo("/api/v1/estacionamentos/check-in")
+                .jsonPath("method").isEqualTo("POST") ;
+    }
+
+    @Test
+    public void criaCheckIn_ComDadosInvalidos_RetornarErrorComStatus422(){
+
+        EstacionamentoCreateDto createDto = EstacionamentoCreateDto.builder()
+            .placa("").marca("").modelo("").cor("").clienteCpf("")
+            .build();
+
+            testClient
+                .post() 
+                .uri("/api/v1/estacionamentos/check-in")
+                .contentType(MediaType.APPLICATION_JSON)
+                .headers(JwtAuthentication.getHeaderAuthentication(testClient,"bia@email.com", "123456")) 
+                .bodyValue(createDto)
+                .exchange()
+                .expectStatus().isEqualTo(422) 
+                .expectBody()
+                .jsonPath("status").isEqualTo("422")
+                .jsonPath("path").isEqualTo("/api/v1/estacionamentos/check-in")
+                .jsonPath("method").isEqualTo("POST") ;
+    }
 }
